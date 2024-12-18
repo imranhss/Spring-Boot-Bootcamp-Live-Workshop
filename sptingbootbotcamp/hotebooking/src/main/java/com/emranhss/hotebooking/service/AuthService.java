3bf824fb86ccb8b3051f6dfa2f03a3ec7a75403f
Muhammad Emran Hossain <emranhss@gmail.com>
2024-12-18 23:50:23 +0600

    Class-8 Auth Service

--- sptingbootbotcamp/hotebooking/src/main/java/com/emranhss/hotebooking/service/AuthService.java
+++ sptingbootbotcamp/hotebooking/src/main/java/com/emranhss/hotebooking/service/AuthService.java
@@ -1,12 +1,15 @@
 package com.emranhss.hotebooking.service;
 
 import com.emranhss.hotebooking.entity.AuthenticationResponse;
+import com.emranhss.hotebooking.entity.Role;
 import com.emranhss.hotebooking.entity.Token;
 import com.emranhss.hotebooking.entity.User;
 import com.emranhss.hotebooking.jwt.JwtService;
 import com.emranhss.hotebooking.repository.TokenRepository;
 import com.emranhss.hotebooking.repository.UserRepository;
+import jakarta.mail.MessagingException;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -62,10 +65,91 @@ public class AuthService {
 
 
     public AuthenticationResponse register(User user) {
+        // We check that Already any user Exists with this email
+        if(userRepository.findByEmail(user.getEmail()).isPresent()){
+
+            return  new AuthenticationResponse(null, "User Already Exists");
+        }
+
+        // Encode user password to save DB
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRole(Role.valueOf("USER"));
+        user.setLock(true);
+        user.setActive(false);
+
+        userRepository.save(user);
+
+        String jwt=jwtService.generateToken(user);
+
+        saveUserToken(jwt, user);
+
+        sendActivationEmail(user);
 
 
-        return null;
+
+        return new AuthenticationResponse(jwt, "User Registration was Successful");
     }
+
+    private void sendActivationEmail(User user) {
+
+        String activationLink="http://localhost:8089/active/"+user.getId();
+        String mailText= " <h2> Dear </h2> "+user.getName()+","
+                +"<p>Pls Click on the following link to confirm your registration </p>"
+                +"<a href=\""+activationLink+"\">Active Account</a>";
+        String subject="Confirm Registration";
+        try{
+            emailService.sendSimpleEmail(user.getEmail(), subject, mailText);
+        }
+        catch (MessagingException e){
+           throw  new RuntimeException();
+
+        }
+
+
+
+    }
+
+
+    public  AuthenticationResponse authencate(User request){
+
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getUsername(),
+                        request.getPassword()
+                )
+        );
+
+        User user=userRepository.findByEmail(request.getEmail()).orElseThrow();
+
+        // Generate Token for Current User
+        String jwt=jwtService.generateToken(user);
+
+        //Remove all existing toke for this user
+        removeAllTokenByUser(user);
+
+        saveUserToken(jwt, user);
+
+        return  new AuthenticationResponse(jwt, "User Login Successful");
+
+    }
+
+
+    public  String activeUser(long id){
+
+        User user=userRepository.findById(id)
+                .orElseThrow(()-> new RuntimeException("User not Found with this ID"));
+
+        if(user !=null){
+            user.setActive(true);
+            userRepository.save(user);
+            return "User Activated Successfully!";
+
+        }else {
+            return  "Invalid Activation Token!";
+        }
+
+    }
+
 
 
 }
